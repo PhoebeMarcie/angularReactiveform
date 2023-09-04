f0dd071f74824644f1e21fc7295bd702f8381dd6
PhoebeMarcie <phibihmarcie@gmail.com>
2023-09-05 02:05:03 +0300

    intitial form

--- src/app/users/contacts/contacts.component.ts
+++ src/app/users/contacts/contacts.component.ts
@@ -4,70 +4,111 @@ import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-contacts',
   templateUrl: './contacts.component.html',
-  styleUrls: ['./contacts.component.css']
+  styleUrls: ['./contacts.component.css'],
 })
 export class ContactsComponent implements OnInit {
-  form:FormGroup;
+  form: FormGroup;
   result;
 
-  constructor(
-    private fb:FormBuilder
-  ) { }
+  constructor(private fb: FormBuilder) {}
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-    this.creatForm();
+  createForm() {
+    this.form = this.fb.group({
+      fName: [null],
+      lName: [null],
+      addresses: this.fb.group(this.addressesForm()),
+      contacts: this.fb.array([this.contactsForm()]),
+    });
   }
-
-  creatForm(){
-    this.form = this.fb.group(
-      {
-        fName: [null],
-        lName: [null],
-        addresses: this.addressForm(),
-        contacts: this.fb.array([this.contactFrom()])
-      }
-    );
+  addressesForm() {
+    return this.fb.group({
+      address1: [null],
+      address2: [null],
+      country: [null],
+      state: [null],
+    });
   }
-
-  addressForm(){
-    return this.fb.group(
-      {
-        address1: [null],
-        address2: [null],
-        country: [null],
-        state: [null]
-      }
-    )
+  get addresses (){
+    return this.form.get("addresses") as FormGroup;
   }
-
-  get addresses(){
-  return this.form.get("addresses") as FormGroup;
-  }
-
-
-  get contacts(){
+  get contacts (){
     return this.form.get("contacts") as FormArray;
-    }
-
-  contactFrom(){
-    return this.fb.group(
-      {
-        phone: [null],
-        email: [null]
-      }
-    );
+  }
+  contactsForm(){
+   return  this.fb.group({
+      phone: [null],
+      email: [null]
+    })
   }
 
   onSave(){
-    console.log(this.form.getRawValue())
+    console.log(this.form.getRawValue());
     this.result = this.form.getRawValue();
   }
-
   addNewContacts(){
-    this.contacts.push(this.contactFrom());
+this.contacts.push(this.contactsForm());
   }
+  // form:FormGroup;
+  // result;
 
-  removeContact(i:Required<number>){
-    this.contacts.removeAt(i);
-  }
+  // constructor(
+  //   private fb:FormBuilder
+  // ) { }
+
+  // ngOnInit(): void {
+  //   this.creatForm();
+  // }
+
+  // creatForm(){
+  //   this.form = this.fb.group(
+  //     {
+  //       fName: [null],
+  //       lName: [null],
+  //       addresses: this.addressForm(),
+  //       contacts: this.fb.array([this.contactFrom()])
+  //     }
+  //   );
+  // }
+
+  // addressForm(){
+  //   return this.fb.group(
+  //     {
+  //       address1: [null],
+  //       address2: [null],
+  //       country: [null],
+  //       state: [null]
+  //     }
+  //   )
+  // }
+
+  // get addresses(){
+  // return this.form.get("addresses") as FormGroup;
+  // }
+
+  // get contacts(){
+  //   return this.form.get("contacts") as FormArray;
+  //   }
+
+  // contactFrom(){
+  //   return this.fb.group(
+  //     {
+  //       phone: [null],
+  //       email: [null]
+  //     }
+  //   );
+  // }
+
+  // onSave(){
+  //   console.log(this.form.getRawValue())
+  //   this.result = this.form.getRawValue();
+  // }
+
+  // addNewContacts(){
+  //   this.contacts.push(this.contactFrom());
+  // }
+
+  // removeContact(i:Required<number>){
+  //   this.contacts.removeAt(i);
+  // }
 }
